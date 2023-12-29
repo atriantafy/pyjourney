296c4cc193b84dfad57b267a40f1893898e7a483
Alexandros <atriantafy@gmail.com>
2023-12-29 17:08:41 +0000

    Refactoring for better readability

--- pyjourney/pyjourney.py
+++ pyjourney/pyjourney.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+"""
+PyJourney: A Python module for automating interactions with the Midjourney bot in Discord.
+
+This module enables automated sending of prompts to the Midjourney bot, handling of
+responses, and image retrieval using Selenium WebDriver. It's designed to simplify
+the process of generating images through the Midjourney bot on Discord.
+
+Classes:
+- PyJourney: Handles bot interactions and image retrieval.
+
+Usage:
+Intended for users automating Midjourney bot interactions, particularly for image
+generation based on textual prompts.
+"""
 import os
 import sys
 import re
@@ -13,26 +27,81 @@ from selenium.webdriver.firefox.options import Options
 import requests
 from PIL import Image
 
+DEFAULT_IMAGE_FETCH_TIMEOUT = 60 * 5
+
 
 class PyJourneyException(Exception):
-    pass
+    """
+    Base exception for errors specific to the PyJourney class.
+
+    This exception is raised for general errors related to the PyJourney class's
+    operations, such as invalid arguments or configuration issues.
+    """
 
 
 class PyJourneyBannedPromptDetectedException(Exception):
-    pass
+    """
+    Exception raised when a banned prompt is detected by the Midjourney bot.
+
+    This exception is specifically used to indicate that the provided prompt to
+    the Midjourney bot has been flagged as banned, prohibiting further processing.
+    """
+
+
+class PyJourneyImageNotFoundInMessage(Exception):
+    """
+    Exception raised when an expected image is not found in a Discord message.
+
+    This exception indicates that an image URL was expected to be found in a
+    specific Discord message, but was not present. This may occur if the bot
+    response does not include an image or if the image extraction fails.
+    """
 
 
 class PyJourneyUnexpectedStatusException(Exception):
-    pass
+    """
+    Exception raised for unexpected status messages from the Midjourney bot
+    which prevents us from retrieving the final image.
+
+    This exception is used when the status message received from the Midjourney bot
+    contains content that is not anticipated or is in an unexpected format, suggesting
+    a possible issue with the bot's response or a change in its output format.
+    """
 
 
-def error_print(s):
+def error_print(s: str):
+    """
+    Print an error message to standard error (stderr).
+
+    This function writes a given string `s` to the stderr and ensures it's immediately flushed,
+    making it useful for logging error messages.
+
+    Args:
+        s (str): The error message to be printed.
+    """
     sys.stderr.write(s)
     sys.stderr.write("\n")
     sys.stderr.flush()
 
 
 class PyJourney:
+    """
+    A class to interface with the Midjourney bot on Discord using Selenium WebDriver.
+
+    This class automates interactions with Discord's Midjourney bot, including logging
+    in to Discord, navigating to the bot's channel, sending messages, and retrieving
+    image URLs.
+
+    Attributes:
+        discord_email (str): Email for Discord. Fetched from environment variables
+                            if not provided.
+        discord_password (str): Password for Discord. Fetched from environment
+                                variables if not provided.
+        discord_midjourney_bot_channel_url (str): URL of the Discord channel with
+                                                the Midjourney bot.
+        headless (bool): If true, runs WebDriver in headless mode.
+    """
+
     def __init__(
         self,
         discord_email: Union[str, None] = None,
@@ -40,6 +109,7 @@ class PyJourney:
         discord_midjourney_bot_channel_url: Union[str, None] = None,
         headless: bool = True,
     ):
+        self._driver = None
         self._discord_email = discord_email or os.environ.get("DISCORD_EMAIL")
         self._discord_password = discord_password or os.environ.get("DISCORD_PASSWORD")
         self._channel_url = discord_midjourney_bot_channel_url or os.environ.get(
@@ -47,18 +117,24 @@ class PyJourney:
         )
         self._headless = headless
 
-    def init_driver(self):
-        # Initialize the Selenium WebDriver for Firefox
+    def _init_driver(self):
+        """
+        Initialize the Selenium WebDriver for Firefox.
+        """
         options = Options()
         if self._headless:
             options.add_argument("--headless")
         self._driver = webdriver.Firefox(options=options)
 
-    def login_to_discord(self):
-        # Navigate to Discord login page and log in
-        self._driver.get("https://discord.com/login")
-        time.sleep(6)  # Wait for page to load
+    def _login_to_discord(self):
+        """
+        Log in to Discord using the provided credentials.
 
+        This method navigates to the Discord login page and performs an automated
+        login using the email and password attributes of the PyJourney class.
+        """
+        self._driver.get("https://discord.com/login")
+        time.sleep(6)
         # Enter email
         email_field = self._driver.find_element(By.NAME, "email")
         email_field.send_keys(self._discord_email)
@@ -67,15 +143,22 @@ class PyJourney:
         password_field = self._driver.find_element(By.NAME, "password")
         password_field.send_keys(self._discord_password)
         password_field.send_keys(Keys.RETURN)
+        time.sleep(5)
 
-        time.sleep(5)  # Wait for Discord to log in
-
-    def start_bot_chat(self):
-        # Navigate to the specific channel where the Midjourney bot is and send a message
+    def _start_bot_chat(self):
+        """
+        Navigate to the Discord channel where the Midjourney bot is located.
+        """
         self._driver.get(self._channel_url)
-        time.sleep(10)  # Wait for channel to load
+        time.sleep(10)
 
-    def send_message_to_midjourney_bot(self, message):
+    def _send_message_to_midjourney_bot(self, message: str):
+        """
+        Send a message to the Midjourney bot in Discord.
+
+        Args:
+            message (str): The message to be sent to the Midjourney bot.
+        """
         message_box = self._driver.find_element(
             By.XPATH, '//div[@role="textbox"][contains(@class,"slateTextArea")]'
         )
@@ -84,18 +167,60 @@ class PyJourney:
         message_box.send_keys(Keys.RETURN)
         time.sleep(6)
 
-    def close_driver(self):
+    def _close_driver(self):
+        """
+        Close the Selenium WebDriver.
+        """
         self._driver.quit()
 
-    def get_image_url_from_message_id(self, message_id):
-        message_element = self._driver.find_element(By.ID, message_id)
-        link_element = message_element.find_element(By.CSS_SELECTOR, "a[href]")
-        image_url = link_element.get_attribute("href")
-        return image_url
+    def _get_image_url_from_message_id(self, message_id: str) -> str:
+        """
+        Retrieve the image URL from a Discord message.
+
+        Args:
+            message_id (str): The ID of the Discord message containing the image link.
+
+        Returns:
+            str: The URL of the image in the specified message.
+        """
+        message_element = None
+        link_element = None
+        image_url = None
+        try:
+            message_element = self._driver.find_element(By.ID, message_id)
+            link_element = message_element.find_element(By.CSS_SELECTOR, "a[href]")
+            image_url = link_element.get_attribute("href")
+            return image_url
+        except Exception as exc:
+            error_msg = f"Could not extract image URL from message_id: {message_id}"
+            if message_element is not None:
+                error_msg += (
+                    "\nmessage_element:\n"
+                    + message_element.get_attribute("outerHTML")
+                    + "\n\n"
+                )
+            if link_element is not None:
+                error_msg += (
+                    "\nlink_element:\n"
+                    + link_element.get_attribute("outerHTML")
+                    + "\n\n"
+                )
+            raise PyJourneyImageNotFoundInMessage(error_msg) from exc
 
     @staticmethod
-    def split_image(image_url):
-        response = requests.get(image_url)
+    def _split_image(image_url: str) -> list:
+        """
+        Split a single image from the given URL into four sub-images.
+
+        This method downloads the image from the provided URL and splits it into four equal parts.
+
+        Args:
+            image_url (str): URL of the image to be split.
+
+        Returns:
+            List[Image]: A list of four PIL Image objects, each representing a quarter of the original image.
+        """
+        response = requests.get(image_url, timeout=DEFAULT_IMAGE_FETCH_TIMEOUT)
         if response.status_code != 200:
             return None
 
@@ -119,24 +244,56 @@ class PyJourney:
         return images
 
     @staticmethod
-    def save_image(image, filename):
-        # Extract the format from the filename
-        format = filename.split(".")[-1].upper()
-        if format != "JPG":
+    def save_image(image: Image, filename: str):
+        """
+        Save a PIL Image object to a file.
+
+        This method saves the given PIL Image object to a file with the specified filename,
+        currently only supporting JPEG format.
+
+        Args:
+            image (Image): The PIL Image object to be saved.
+            filename (str): The filename for the saved image, including the '.jpg' extension.
+
+        Raises:
+            PyJourneyException: If the file format is not JPEG.
+        """
+        if filename.split(".")[-1].upper() != "JPG":
             raise PyJourneyException("Can only save in .jpg format!")
-        # Save the image with the extracted format
         image.save(filename, format="JPEG")
 
-    def get_element_text_by_id(self, element_id):
+    def _get_element_text_by_id(self, element_id: str) -> str:
+        """
+        Retrieve the text from a webpage element identified by its ID.
+
+        Args:
+            element_id (str): The ID of the element from which text is to be extracted.
+
+        Returns:
+            str: The text content of the specified element.
+        """
         element = self._driver.find_element(By.ID, element_id)
         text = element.text
         return text
 
     @staticmethod
-    def handle_status_text(status_text: str):
+    def _handle_status_text(status_text: str):
+        """
+        Analyze and respond to the status text from the Midjourney bot.
+
+        This method processes the status text received from the bot, checking for
+        any indications of a banned prompt or other issues. It raises specific
+        exceptions for different error conditions detected in the status text.
+
+        Args:
+            status_text (str): The status text to be analyzed.
+
+        Raises:
+            PyJourneyBannedPromptDetectedException: If the prompt is detected as banned.
+            PyJourneyUnexpectedStatusException: If the status text contains unexpected content.
+        """
         if "banned" in status_text.lower():
             raise PyJourneyBannedPromptDetectedException(status_text)
-        # pattern = r"@(\S+) \((.*)\)"
         pattern = r"@(\S+) \(([^()]+)\)"
         match = re.search(pattern, status_text)
         if match:
@@ -146,29 +303,42 @@ class PyJourney:
         else:
             raise PyJourneyUnexpectedStatusException(status_text)
 
-    def get_4_images(self, prev_last_message_id):
-        process_started_message_id = self.get_last_message_id()
-        status_text = self.get_element_text_by_id(process_started_message_id)
-        PyJourney.handle_status_text(status_text)
+    def _get_4_images(self, prev_last_message_id: str) -> list:
+        """
+        Retrieve up to four images generated by the Midjourney bot.
+
+        This method waits for new messages from the Midjourney bot after a prompt
+        is sent, extracts the image URLs from the messages, and downloads and splits
+        the images.
+
+        Args:
+            prev_last_message_id (str): The ID of the last message before sending the prompt.
+
+        Returns:
+            List[Image]: A list of up to four PIL Image objects generated by the Midjourney bot.
+        """
+        process_started_message_id = self._get_last_message_id()
+        status_text = self._get_element_text_by_id(process_started_message_id)
+        PyJourney._handle_status_text(status_text)
         while process_started_message_id == prev_last_message_id:
             time.sleep(0.5)
-            process_started_message_id = self.get_last_message_id()
-            status_text = self.get_element_text_by_id(process_started_message_id)
-            PyJourney.handle_status_text(status_text)
+            process_started_message_id = self._get_last_message_id()
+            status_text = self._get_element_text_by_id(process_started_message_id)
+            PyJourney._handle_status_text(status_text)
 
         error_print(f"Process started ({process_started_message_id}) ...")
-        result_message_id = self.get_last_message_id()
+        result_message_id = self._get_last_message_id()
         while result_message_id == process_started_message_id:
             time.sleep(0.5)
-            result_message_id = self.get_last_message_id()
+            result_message_id = self._get_last_message_id()
         error_print(f"Image Generation Complete ({result_message_id})")
 
-        image_url = self.get_image_url_from_message_id(result_message_id)
+        image_url = self._get_image_url_from_message_id(result_message_id)
         error_print(f"Extracted Image: {image_url}")
-        images = PyJourney.split_image(image_url=image_url)
+        images = PyJourney._split_image(image_url=image_url)
         return images
 
-    def get_last_message_id(self):
+    def _get_last_message_id(self):
         messages = self._driver.find_elements(
             By.XPATH, "//li[starts-with(@id, 'chat-messages-')]"
         )
@@ -184,25 +354,52 @@ class PyJourney:
         num_images: int,
         filename_prefix: Union[str, None] = None,
         aspect_ratio: str = "16:9",
-    ):
+    ) -> list:
+        """
+        Generate and retrieve images based on a given prompt using the Midjourney bot.
+
+        This method sends a prompt to the Midjourney bot, waits for image generation,
+        and saves the resulting images with an optional filename prefix. It handles
+        connecting to Discord, navigating to the bot channel, sending the prompt,
+        and processing the images.
+
+        Args:
+            prompt (str): The prompt for the Midjourney bot.
+            num_images (int): Number of images to generate (1-4).
+            filename_prefix (Union[str, None]): Prefix for saved image filenames.
+            aspect_ratio (str): Aspect ratio for images (e.g., "16:9").
+
+        Returns:
+            List[Image]: PIL Image objects representing the generated images.
+
+        Raises:
+            PyJourneyException: If num_images is outside 1-4, or if Discord
+                                credentials are not set.
+            PyJourneyBannedPromptDetectedException: If the prompt is banned.
+            PyJourneyUnexpectedStatusException: If unexpected content in bot's
+                                                status message.
+            Selenium WebDriver exceptions: Various exceptions from WebDriver
+                                        operations, like connection issues.
+        """
+
         if num_images < 0 or num_images > 4:
             raise PyJourneyException("num_images can be between 1 and 4")
         if not self._discord_email or not self._discord_password:
             raise PyJourneyException("Discord credentials are not set")
         error_print("Connecting to Discord ...")
-        self.init_driver()
+        self._init_driver()
         try:
-            self.login_to_discord()
+            self._login_to_discord()
 
-            self.start_bot_chat()
+            self._start_bot_chat()
 
-            last_message_id = self.get_last_message_id()
+            last_message_id = self._get_last_message_id()
 
-            self.send_message_to_midjourney_bot(
+            self._send_message_to_midjourney_bot(
                 f"/imagine prompt: {prompt} --ar {aspect_ratio}"
             )
 
-            images = self.get_4_images(last_message_id)
+            images = self._get_4_images(last_message_id)
             if filename_prefix is not None:
                 for i in range(num_images):
                     filename = f"{filename_prefix}{i}.jpg"
@@ -210,10 +407,10 @@ class PyJourney:
                     error_print(f"Saved image in {filename}")
             return images[:num_images]
         finally:
-            self.close_driver()
+            self._close_driver()
 
 
-def check_env_vars():
+def _check_env_vars():
     for var_name in [
         "DISCORD_EMAIL",
         "DISCORD_PASSWORD",
@@ -248,7 +445,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    check_env_vars()
+    _check_env_vars()
 
     mj_api = PyJourney()
 
